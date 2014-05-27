d08dc1c6508abf12d998a6c516e79e6ff7f7c8f1
Ge Jin <jinwar@gmail.com>
2014-05-26 21:58:53 -0400

    test

--- TPW_vel_err.m
+++ TPW_vel_err.m
@@ -4,8 +4,6 @@ function errs_all = TPW_vel_err(para,event_parastr,event_data)
 errs_all = [];
 
 for ie = 1:length(event_data)
-	event_para = para((ie-1)*3+3:(ie-1)*3+5);
-	event_parastr(ie) = para2str(event_para,event_data(ie));
 	event_parastr(ie).v1 = para(1);
 	event_parastr(ie).v2 = para(2);
 	errs = TPW_err_array(event_parastr(ie),event_data(ie));
